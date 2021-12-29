da671afc36b71556a336c3ac35ede087dbbb3d8c
Philipp Eder <philipp.eder@posteo.net>
2021-12-29 22:24:52 +0100

    Refactoring: move from Calculation to various files

---
+++ SnackDog/BasePlan.swift
@@ -0,0 +1,73 @@
+import Foundation
+
+class FoodBasePlan: Hashable, Identifiable {
+    
+    let id: UUID
+    let name: String
+    let needs: [Need]
+    
+    init(id: UUID = UUID(), name: String, needs: [Need]) {
+        self.id = id
+        self.needs = needs
+        self.name = name
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(name)
+        hasher.combine(needs)
+    }
+    
+    static func == (lhs: FoodBasePlan, rhs: FoodBasePlan) -> Bool {
+        return lhs.id == rhs.id &&
+        lhs.name == rhs.name &&
+        lhs.needs == rhs.needs
+    }
+    
+    static let summarizedInsides: FoodBasePlan = FoodBasePlan(name: "Summarized insides", needs: [
+        .scallop,
+        .rumen,
+        .insides,
+        .bones,
+        .vegetables,
+        .fruits,
+        .algaePowder,
+        .codliverOild,
+        .omega3Oil,
+        .fat,
+    ])
+    
+    static func onlyWeakBones(basedOn: FoodBasePlan) -> FoodBasePlan {
+        
+        let needs: [Need] = basedOn.needs.map {
+            
+            if $0 == Need.rumen {
+                return $0.copy(percentage:0.15)
+            }
+            if $0 == Need.bones {
+                return $0.copy(percentage: 0.20)
+            }
+            return $0
+        }
+        return FoodBasePlan(name: "\(basedOn.name) only weak bones (e.g. chicken)", needs: needs)
+    }
+    static let summarizedInsidesOnlyWeakBones: FoodBasePlan = onlyWeakBones(basedOn: FoodBasePlan.summarizedInsides)
+    
+    static let separatedInsides: FoodBasePlan = FoodBasePlan(name: "Insides", needs: [
+        .scallop,
+        .rumen,
+        .liver,
+        .lung,
+        .heart,
+        .kidney,
+        .spleen,
+        .bones,
+        .vegetables,
+        .fruits,
+        .algaePowder,
+        .codliverOild,
+        .omega3Oil,
+        .fat,
+    ])
+    static let separatedInsidesOnlyWeakBones: FoodBasePlan = onlyWeakBones(basedOn: FoodBasePlan.separatedInsides)
+}
---
+++ SnackDog/DogSize.swift
@@ -0,0 +1,67 @@
+import Foundation
+
+enum DogSize: String, Hashable {
+    case small, medium, large
+    
+}
+
+extension DogSize {
+    func factor(_ birthDate: Date) -> Double {
+        return DogSizeFactor.byDogSize(self).factor(birthDate)
+    }
+}
+
+class DogSizeFactor {
+    let factor: [(Calendar.Component?, Int, Double)]
+    init(factor: [(Calendar.Component?, Int, Double)]) {
+        self.factor = factor
+    }
+    
+    func factor(_ birthDate: Date) -> Double {
+        let calendar = Calendar.current
+        let now = Date()
+        for t in factor {
+            if let compontent = t.0 {
+                let age = calendar.dateComponents([compontent], from: now).value(for: compontent)!
+                if age < t.1 {
+                    return t.2
+                }
+            } else {
+                return t.2
+            }
+        }
+        return factor.last?.2 ?? 0.02
+    }
+    
+    static let small = DogSizeFactor(factor: [
+        (.weekOfYear, 16, 0.051),
+        (.weekOfYear, 24, 0.045),
+        (.month, 10, 0.032),
+        (nil, 0, 0.03),
+    ])
+    
+    static let medium = DogSizeFactor(factor:[
+        (.weekOfYear, 16, 0.054),
+        (.weekOfYear, 24, 0.048),
+        (.weekOfYear, 34, 0.038),
+        (.month, 12, 0.036),
+        (.month, 18, 0.03),
+        (nil, 0, 0.025),
+    ])
+    
+    static let large = DogSizeFactor(factor: [
+        (.weekOfYear, 16, 0.06),
+        (.weekOfYear, 24, 0.054),
+        (.weekOfYear, 38, 0.048),
+        (.month, 16, 0.042),
+        (.month, 24, 0.036),
+        (nil, 0, 0.02),
+    ])
+    
+    static let all = [DogSize.small: small, DogSize.medium: medium, DogSize.large: large]
+    
+    static func byDogSize(_ size: DogSize) -> DogSizeFactor {
+        return .all[size] ?? .small
+    }
+    
+}
